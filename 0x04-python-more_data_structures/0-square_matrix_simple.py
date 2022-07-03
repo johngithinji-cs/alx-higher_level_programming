a0a88ac99b2c5ab0236fc855aaff8f9c8ac8f544
johngithinji-cs <60788095+johngithinji-cs@users.noreply.github.com>
2022-07-03 13:04:35 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# 0-square_matrix_simple.py
-
-
 def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
