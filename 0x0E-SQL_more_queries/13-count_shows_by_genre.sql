88c42ef2e662f8b8a80c1207f4fb820d868c09ad
johngithinji-cs <mathaijohn97@gmail.com>
2022-08-20 18:48:51 +0300

    changes

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 -- lists all rows of a database that don't have one column
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_genres.name AS genre, COUNT(*) As 'number_of_shows'
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genres_id
+GROUP BY genre
+ORDER BY number_shows DESC;
