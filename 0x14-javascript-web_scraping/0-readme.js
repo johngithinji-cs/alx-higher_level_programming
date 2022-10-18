88633f56aea41926ce5e34b894080de3b117a6a4
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-18 19:56:00 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // JS Script
 let fs = require('fs');
-fs.readFile(process.argv[2], function (err, contents) {
-  if (!err) {
-    console.log(contents.toString().trim());
-  } else {
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (err) {
     console.log(err);
+  } else {
+    console.log(contents);
   }
 });
